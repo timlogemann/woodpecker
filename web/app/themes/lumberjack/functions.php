c15bbc5a2e925a719d6bc0afee18ba026554cd3c
Tim Logemann <tim@logemann.nu>
2020-09-21 14:08:06 +0200

    enqueue dist assets

--- web/app/themes/lumberjack/functions.php
+++ web/app/themes/lumberjack/functions.php
@@ -12,5 +12,18 @@ $lumberjack->bootstrap();
 // Import our routes file
 require_once('routes.php');
 
+require_once('acf/index.php');
+
 // Set global params in the Timber context
 add_filter('timber_context', [$lumberjack, 'addToContext']);
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('lumberjack/main.css', get_template_directory_uri() . '/dist/styles/main.css', false, null);
+    wp_enqueue_script('lumberjack/main.js', get_template_directory_uri() . '/dist/scripts/main.js', ['jquery'], null, true);
+}, 100);
+
+/**
+ * Customizer JS
+ */
+add_action('customize_preview_init', function () {
+    wp_enqueue_script('lumberjack/customizer.js', get_template_directory_uri() . '/dist/scripts/customizer.js', ['customize-preview'], null, true);
+});
